6bc82a151d389bbe2e883be1b2c5045d3787a762
Peter Henderson <Breakend@web024007.wireless.mcgill.ca>
2014-01-15 22:41:07 -0500

    and another

--- config/config.js
+++ config/config.js
@@ -8,7 +8,7 @@ module.exports = {
 		facebook: {
 			clientID: "1399029103680176",
 			clientSecret: "1604e9a6d6dd3720c865975b09446f3c",
-			callbackURL: "http://agile-ridge-8948.herokuapp.com/auth/facebook/callback"
+			callbackURL: "http://agile-ridge-8948.herokuapp.com/auth/facebook/callback",
 			profileFields: ['id', 'emails', 'displayName', 'photos']
 
 		}
